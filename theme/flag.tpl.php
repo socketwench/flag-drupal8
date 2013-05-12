1363f2eb36cbce513b4e5dbe6598da3faceabd67
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2013-05-12 07:28:58 +0100

    Issue #1892930 by joachim, chrisjlee: Added placement of flag links as pseudofields.

--- theme/flag.tpl.php
+++ theme/flag.tpl.php
@@ -27,6 +27,8 @@
  *   flagged. If displaying to the user immediately after flagging, this value
  *   will be boolean TRUE. This is usually used in conjunction with immedate
  *   JavaScript-based toggling of flags.
+ * - $needs_wrapping_element: Determines whether the flag displays a wrapping
+ *   HTML DIV element.
  *
  * Template suggestions available, listed from the most specific template to
  * the least. Drupal will use the most specific template it finds:
@@ -37,6 +39,9 @@
  * advanced theming you may have to remove all the whitespace.
  */
 ?>
+<?php if ($needs_wrapping_element): ?>
+  <div class="flag-outer flag-outer-<?php print $flag_name_css; ?>">
+<?php endif; ?>
 <span class="<?php print $flag_wrapper_classes; ?>">
   <?php if ($link_href): ?>
     <a href="<?php print $link_href; ?>" title="<?php print $link_title; ?>" class="<?php print $flag_classes ?>" rel="nofollow"><?php print $link_text; ?></a><span class="flag-throbber">&nbsp;</span>
@@ -49,3 +54,6 @@
     </span>
   <?php endif; ?>
 </span>
+<?php if ($needs_wrapping_element): ?>
+  </div>
+<?php endif; ?>
