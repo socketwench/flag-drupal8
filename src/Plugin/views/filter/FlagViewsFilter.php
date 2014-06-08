8fdae2cc778d6ac05ceb67fdbd05096a3049562b
socketwench <tess@deninet.com>
2014-06-08 15:30:37 -0500

    Working flag views filter.

---
+++ src/Plugin/views/filter/FlagViewsFilter.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @file
+ */
+
+namespace Drupal\flag\Plugin\views\filter;
+
+use Drupal\views\Plugin\views\filter\BooleanOperator;
+
+/**
+ * Class FlagViewsFilter
+ * @package Drupal\flag\Plugin\views\filter
+ *
+ * @ViewsFilter("flag_filter")
+ */
+class FlagViewsFilter extends BooleanOperator {
+
+  public function defineOptions() {
+    $options = parent::defineOptions();
+    $options['value'] = array('default' => 1);
+    return $options;
+  }
+
+  public function buildOptionsForm(&$form, &$form_state) {
+    parent::buildOptionsForm($form, $form_state);
+    $form['value']['#type'] = 'radios';
+    $form['value']['#title'] = t('Status');
+    $form['value']['#options'] = array(
+      1 => t('Flagged'),
+      0 => t('Not flagged'),
+      // @todo Find out what in the hell filter type ALL is supposed to do.
+      //'All' => t('All'),
+    );
+    $form['value']['#default_value'] = empty($this->options['value']) ? '0' : $this->options['value'];
+    $form['value']['#description'] = '<p>' . t('This filter is only needed if the relationship used has the "Include only flagged content" option <strong>unchecked</strong>. Otherwise, this filter is useless, because all records are already limited to flagged content.') . '</p><p>' . t('By choosing <em>Not flagged</em>, it is possible to create a list of content <a href="@unflagged-url">that is specifically not flagged</a>.', array('@unflagged-url' => 'http://drupal.org/node/299335')) . '</p>';
+  }
+
+
+  public function query() {
+    $this->ensureMyTable();
+
+    $operator = $this->options['value'] ? 'IS NOT' : 'IS';
+    $operator .= ' NULL';
+
+    $this->query->addWhere($this->options['group'], "$this->tableAlias.uid", NULL, $operator);
+  }
+}
