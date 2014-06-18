9dcaa0dfb49a1529b56e701c366f27699f67bc65
socketwench <tess@deninet.com>
2014-06-17 20:40:24 -0500

    Plugin manager changes due to https://drupal.org/node/2281905

--- src/ActionLinkPluginManager.php
+++ src/ActionLinkPluginManager.php
@@ -23,7 +23,7 @@ class ActionLinkPluginManager extends DefaultPluginManager {
   public function __construct(\Traversable $namespaces, CacheBackendInterface $cache_backend, LanguageManager $language_manager, ModuleHandlerInterface $module_handler) {
     parent::__construct('Plugin/ActionLink', $namespaces, $module_handler, 'Drupal\flag\Annotation\ActionLinkType');
     $this->alterInfo('flag_link_type_info');
-    $this->setCacheBackend($cache_backend, $language_manager, 'flag_link_type_plugins');
+    $this->setCacheBackend($cache_backend, 'flag_link_type_plugins');
   }
 
   public function getAllLinkTypes() {
