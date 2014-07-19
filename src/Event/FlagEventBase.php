2f36f7bf4a1a91e6ddfdf468dd89c69622e03513
Martin Frances <martinfrances107@hotmail.com>
2014-07-19 16:35:50 +0100

    applied coding standard comments to the Events

--- src/Event/FlagEventBase.php
+++ src/Event/FlagEventBase.php
@@ -1,21 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tess
- * Date: 6/29/14
- * Time: 6:09 PM
+ * @file
+ * Contains \Drupal\flag\Event\FlagEventBase.
  */
 
 namespace Drupal\flag\Event;
 
-
 use Drupal\flag\FlagInterface;
 use Symfony\Component\EventDispatcher\Event;
 
+/**
+ * Base Event from which other flag event are defined.
+ */
 abstract class FlagEventBase extends Event{
 
+  /**
+   * The Flag in question.
+   *
+   * @var \Drupal\flag\FlagInterface
+   */
   protected $flag;
 
+  /**
+   * @param \Drupal\flag\FlagInterface $flag
+   *   The flag to be acted upon.
+   */
   public function __construct(FlagInterface $flag) {
     $this->flag = $flag;
   }
