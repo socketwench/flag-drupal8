89605fe9034df6a18b308c369dad5c673eb08557
socketwench <tess@deninet.com>
2014-09-09 20:17:26 -0500

    Initial broken Flagging field entry link type.

---
+++ src/Form/FlaggingForm.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file
+ * Contains the flagging form.
+ */
+
+namespace Drupal\flag\Form;
+
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides the flagging form for field entry.
+ *
+ * @package Drupal\flag\Form
+ */
+class FlaggingForm extends ContentEntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildForm($form, $form_state);
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submit(array $form, FormStateInterface $form_state) {
+    $entity = parent::submit($form, $form_state);
+    // $form_state->setRedirect('content_entity_example.contact_list');
+
+    return $entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $entity = $this->entity;
+    $entity->save();
+  }
+}
