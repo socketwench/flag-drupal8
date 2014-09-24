3e14aac32ac752a0df9b0853fb46a0049d50cd2c
socketwench <tess@deninet.com>
2014-09-24 12:49:53 -0500

    Added documentation to FlagPermissions.

--- src/FlagPermissions.php
+++ src/FlagPermissions.php
@@ -1,19 +1,32 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tess
- * Date: 9/23/14
- * Time: 5:02 PM
+ * @file
+ * Provides the FlagPermissions class.
  */
 
 namespace Drupal\flag;
 
-
+/**
+ * Provides dynamic permissions for defined flags.
+ *
+ * @package Drupal\flag
+ */
 class FlagPermissions {
+
+  /**
+   * Returns an array of dynamic flag permissions.
+   *
+   * @return array
+   *   An array of permissions.
+   *
+   * @see Drupal\flag\FlagInterface::getPermissions().
+   */
   public function permissions() {
     $permissions = [];
 
+    // Get a list of flags from the FlagService.
     $flags = \Drupal::service('flag')->getFlags();
+
     // Provide flag and unflag permissions for each flag.
     foreach ($flags as $flag_name => $flag) {
       $permissions += $flag->getPermissions();
